f9c8c7c8cfbedaf670a670cba30b4b5eceb3a753
wbbeyourself <beyourselfwb@163.com>
2018-08-11 13:40:26 +0800

    add friendly process print

--- train/char_cnn.py
+++ train/char_cnn.py
@@ -2,14 +2,18 @@
 
 import os
 import gc
+
 #
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import tensorflow as tf
+
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
 import sys
+from time import time
+
 sys.path.append("..")
 import pandas as pd
 import numpy as np
@@ -28,6 +32,7 @@ val = pd.read_csv(Config.test_data_path, sep='\t')
 val_label = to_categorical(val.label)
 
 load_val = True
+# load_val = False
 batch_size = 64
 model_name = "char_cnn"
 trainable_layer = ["embedding"]
@@ -35,22 +40,28 @@ train_batch_generator = char_cnn_train_batch_generator
 
 print("Load Val Data")
 if load_val:
-    val_char_seq = pickle.load(open(Config.cache_dir + '/g_val_char_seq_%s.pkl'%Config.char_seq_maxlen, "rb"))
+    val_char_seq = pickle.load(open(Config.cache_dir + '/g_val_char_seq_%s.pkl' % Config.char_seq_maxlen, "rb"))
 else:
     val_char_seq = get_char_seq(val.content.values)
     gc.collect()
-    pickle.dump(val_char_seq, open(Config.cache_dir + '/g_val_char_seq_%s.pkl'%Config.char_seq_maxlen, "wb"))
+    pickle.dump(val_char_seq, open(Config.cache_dir + '/g_val_char_seq_%s.pkl' % Config.char_seq_maxlen, "wb"))
 print(np.shape(val_char_seq))
-
 
 print("Load Char embed")
 char_embed_weight = pickle.load(open(Config.char_embed_path, "rb"))
 
 model = get_textcnn(Config.char_seq_maxlen, char_embed_weight)
 from keras.utils.vis_utils import plot_model
-plot_model(model, to_file= model_name+ '.png',show_shapes=True)
+
+plot_model(model, to_file=model_name + '.png', show_shapes=True)
+
+best_f1 = 0
+
+t0 = time()
 
 for i in range(15):
+
+    print('*********Epoch %s/%s Start************' % (i + 1, 15))
     if i == 8:
         K.set_value(model.optimizer.lr, 0.0001)
     if i == 12:
@@ -59,12 +70,40 @@ for i in range(15):
     model.fit_generator(
         train_batch_generator(train.content.values, train.label.values, batch_size=batch_size),
         epochs=1,
-        steps_per_epoch=int(train.shape[0]/batch_size),
-        validation_data = (val_char_seq, val_label)
+        steps_per_epoch=int(train.shape[0] / batch_size),
+        validation_data=(val_char_seq, val_label)
     )
     pred = np.squeeze(model.predict(val_char_seq))
     pre, rec, f1 = score(pred, val_label)
+    f1_mean = np.mean(f1)
+
+    # set numpy print precision
+    np.set_printoptions(precision=5, suppress=True)
+
     print("precision", pre)
     print("recall", rec)
     print("f1_score", f1)
-    model.save(Config.cache_dir + '/char_cnn/dp_embed_%s_epoch_%s_%s.h5' % (model_name, i, f1))
+    print("f1_mean", f1_mean)
+
+    t1 = time()
+
+    secs = int(t1 - t0)
+    h = secs // 3600
+    m = (secs - h * 3600) // 60
+
+    print('\n\ntime cost %shour %sminute\n\n' % (h, m))
+
+
+    print('*********Epoch %s/%s End************' % (i + 1, 15))
+
+    if f1_mean > best_f1:
+        best_f1 = f1_mean
+        model.save(Config.cache_dir + '/char_cnn/%s_epoch_%s_%s_%.7s.h5' % (model_name, i, f1, best_f1))
+
+t1 = time()
+
+secs = int(t1 - t0)
+h = secs // 3600
+m = (secs - h * 3600) // 60
+
+print('time cost %shour %sminute' % (h, m))
