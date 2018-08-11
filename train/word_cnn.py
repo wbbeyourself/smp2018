9180c20646e7404a6957e6c644d9e5ee9679b719
wbbeyourself <beyourselfwb@163.com>
2018-08-11 11:14:10 +0800

    format

--- train/word_cnn.py
+++ train/word_cnn.py
@@ -2,15 +2,18 @@
 import os
 
 import gc
+
 #
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import tensorflow as tf
+
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
 import sys
+
 sys.path.append("..")
 import pandas as pd
 import numpy as np
@@ -18,7 +21,6 @@ import pickle
 import keras
 import keras.backend as K
 from keras.models import load_model
-
 
 from init.config import Config
 from my_utils.data_preprocess import to_categorical, word_cnn_train_batch_generator, get_word_seq
@@ -40,11 +42,11 @@ trainable_layer = ["embedding"]
 train_batch_generator = word_cnn_train_batch_generator
 
 if load_val:
-    val_word_seq = pickle.load(open(Config.cache_dir + "/g_val_word_seq_%s.pkl"%Config.word_seq_maxlen, "rb"))
+    val_word_seq = pickle.load(open(Config.cache_dir + "/g_val_word_seq_%s.pkl" % Config.word_seq_maxlen, "rb"))
 else:
     val_word_seq = get_word_seq(val.content.values)
     gc.collect()
-    pickle.dump(val_word_seq, open(Config.cache_dir + "/g_val_word_seq_%s.pkl"%Config.word_seq_maxlen, "wb"))
+    pickle.dump(val_word_seq, open(Config.cache_dir + "/g_val_word_seq_%s.pkl" % Config.word_seq_maxlen, "wb"))
 print(np.shape(val_word_seq))
 
 print("Load Word")
@@ -60,21 +62,21 @@ for i in range(10):
     if i == 6:
         K.set_value(model.optimizer.lr, 0.0001)
     if i == 4:
-      for l in trainable_layer:
-          model.get_layer(l).trainable = True
+        for l in trainable_layer:
+            model.get_layer(l).trainable = True
     model.fit_generator(
         train_batch_generator(train.content.values, train.label.values, batch_size=batch_size),
         epochs=1,
-        steps_per_epoch=int(train.shape[0]/batch_size),
+        steps_per_epoch=int(train.shape[0] / batch_size),
         validation_data=(val_word_seq, val_label)
     )
     pred = np.squeeze(model.predict(val_word_seq))
     pre, rec, f1 = score(pred, val_label)
-    
+
     # set numpy print precision
     np.set_printoptions(precision=5, suppress=True)
-    
+
     print("precision", pre)
     print("recall", rec)
     print("f1_score", f1)
-    model.save(Config.cache_dir + '/cnn/%s_epoch_%s_%s_%.5s.h5'%(model_name, i, f1, np.mean(f1)))
+    model.save(Config.cache_dir + '/cnn/%s_epoch_%s_%s_%.5s.h5' % (model_name, i, f1, np.mean(f1)))
